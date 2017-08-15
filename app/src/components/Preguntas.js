7c5648adec3af26cf2497162e64c5e80f6b3ae6c
ejarquin <ejarquin@thedataage.com>
2017-08-14 19:30:40 -0600

    agregue estilos para los radio button según materialize

--- app/src/components/Preguntas.js
+++ app/src/components/Preguntas.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
 import config from '../fb_config'
-import { Carousel } from 'react-materialize';
+import { Carousel, Row, Input } from 'react-materialize';
 
 if (!firebase.apps.length)
   firebase.initializeApp(config)
@@ -26,126 +26,61 @@ class Preguntas extends Component {
 		else {
 			return <div>
 				<Carousel options={{ fullWidth: true }}>
-	<div className='red'>
-		<h2>First Panel</h2>
-		<p className='white-text'>This is your first panel</p>
-	</div>
-	<div className='amber'>
-		<h2>Second Panel</h2>
-		<p className='white-text'>This is your second panel</p>
-	</div>
-	<div className='green'>
-		<h2>Third Panel</h2>
-		<p className='white-text'>This is your third panel</p>
-	</div>
-	<div className='blue'>
-		<h2>Fourth Panel</h2>
-		<p className='white-text'>This is your fourth panel</p>
-	</div>
+				<div className='red'>
+					<h2>First Panel</h2>
+					<p className='white-text'>This is your first panel</p>
+				</div>
+				<div className='amber'>
+					<h2>Second Panel</h2>
+					<p className='white-text'>This is your second panel</p>
+				</div>
+				<div className='green'>
+					<h2>Third Panel</h2>
+					<p className='white-text'>This is your third panel</p>
+				</div>
+				<div className='blue'>
+					<h2>Fourth Panel</h2>
+					<p className='white-text'>This is your fourth panel</p>
+				</div>
 			</Carousel>
 				<form id="test" onSubmit={this.sendDataTest} >
 					<div>
 						<p>¿Qué es git</p>
-						<div>
-							<label htmlFor="p1r1">Un control de versiones</label>
-							<input type="radio" name="p1" value="r1" id="p1r1"  checked />
-						</div>
-						<div>
-							<label htmlFor="p1r2">Un gestor de repositorios</label>
-							<input type="radio" name="p1" value="r2" id="p1r2" />
-						</div>
-						<div>
-							<label htmlFor="p1r3">Gestor de repositorios local</label>
-							<input type="radio" name="p1" value="r3" id="p1r3"/>
-						</div>
-						<div>
-							<label htmlFor="p1r4">Ninguna de las anteriores</label>
-							<input type="radio" name="p1" value="r4" id="p1r4" />
-						</div>
-					</div>
+						<Input name='p1' type='radio' value='r1' label='Un control de versiones' defaultChecked='checked' />
+						<Input name='p1' type='radio' value='r2' label='Un gestor de repositorios' />
+						<Input name='p1' type='radio' value='r3' label='Gestor de repositorios local' />
+						<Input name='p1' type='radio' value='r4' label='Ninguna de las anteriores<' />
+					</div>	
+
 					<div>
 						<p>¿Cómo funciona git?</p>
-						<div>
-							<label htmlFor="p2r1">Crea una copia exacta de todos los archivos modificados</label>
-							<input type="radio" name="p2" value="r1" id="p2r1" checked />
-						</div>
-						<div>
-							<label htmlFor="p2r2">Crea una copia exacta de todos los archivos incluyendo los no modificados</label>
-							<input type="radio" name="p2" value="r2" id="p2r2" />
-						</div>
-						<div>
-							<label htmlFor="p2r3">Crea una copia del archivo modificado y guarda una referencia a esa modificación</label>
-							<input type="radio" name="p2" value="r3" id="p2r3" />
-						</div>
-						<div>
-							<label htmlFor="p2r4">Crea un imagen de el estado del proyecto y guarda una referencia a esa imagen</label>
-							<input type="radio" name="p2" value="r4" id="p2r4" />
-						</div>
-						<div>
-							<label htmlFor="p2r5">Crea una imagen solo del archivo modificado y guarda una referencia a esa imagen</label>
-							<input type="radio" name="p2" value="r5" id="p2r5" />
-						</div>
-						<div>
-							<label htmlFor="p2r6">Todas las anteriores son correctas</label>
-							<input type="radio" name="p2" value="r6" id="p2r6" />
-						</div>
+						<Input name='p2' type='radio' value='r1' label='Crea una copia exacta de todos los archivos modificados' defaultChecked='checked' />
+						<Input name='p2' type='radio' value='r2' label='Crea una copia exacta de todos los archivos incluyendo los no modificados' />
+						<Input name='p2' type='radio' value='r3' label='Crea una copia del archivo modificado y guarda una referencia a esa modificación' />
+						<Input name='p2' type='radio' value='r3' label='Crea un imagen de el estado del proyecto y guarda una referencia a esa imagen<' />
+						<Input name='p2' type='radio' value='r5' label='Crea una imagen solo del archivo modificado y guarda una referencia a esa imagen' />
+						<Input name='p2' type='radio' value='r6' label='Todas las anteriores son correctas' />
 					</div>
 					<div>
 						<p>Los 4 estados de git son</p>
-						<div>
-							<label htmlFor="p3r1">Creación, modificación, preparación y confirmación</label>
-							<input type="radio" name="p3" value="r1"  id="p3r1"  checked />
-						</div>
-						<div>
-							<label htmlFor="p3r2">Modificación, preparación, confirmación y elminación</label>
-							<input type="radio" name="p3" value="r2" id="p3r2" />
-						</div>
-						<div>
-							<label htmlFor="p3r3">No son 4 estados, son 5, Creación, modificación, preparación, confirmación, y eliminación</label>
-							<input type="radio" name="p3" value="r3" id="p3r3" />
-						</div>
-						<div>
-							<label htmlFor="p3r4">No son 4 estados, son 3, modificación, preparación y confirmación</label>
-							<input type="radio" name="p3" value="r4" id="p3r4" />
-						</div>
+						<Input name='p3' type='radio' value='r2' label='Creación, modificación, preparación y confirmación' defaultChecked='checked' />
+						<Input name='p3' type='radio' value='r3' label='Modificación, preparación, confirmación y elminación' />
+						<Input name='p3' type='radio' value='r4' label='No son 4 estados, son 5, Creación, modificación, preparación, confirmación, y eliminación' />
+						<Input name='p3' type='radio' value='r5' label='No son 4 estados, son 3, modificación, preparación y confirmación' />
 					</div>
 					<div>
 						<p>Seleccione la afirmación errónea</p>
-						<div>
-							<label htmlFor="p4r1">Si modifico un archivo, puedo recuperar el estado anterior</label>
-							<input type="radio" name="p4" value="r1" id="p4r1" checked />
-						</div>
-						<div>
-							<label htmlFor="p4r2">Si elimino un archivo, lo puedo recuperar al moverme un estado anterior del proyecto</label>
-							<input type="radio" name="p4" value="r2" id="p4r2" />
-						</div>
-						<div>
-							<label htmlFor="p4r3">Hay 3 estados que conforman el flujo de trabajo local de git</label>
-							<input type="radio" name="p4" value="r3" id="p4r3" />
-						</div>
-						<div>
-							<label htmlFor="p4r4">Git guarda el nombre del archivo y el código hash de sus contenidos para identifar los cambios</label>
-							<input type="radio" name="p4" value="r4" id="p4r4" />
-						</div>
+						<Input name="p4" type="radio" value="r1" label="Si modifico un archivo, puedo recuperar el estado anterior<" defaultChecked='checked' />
+						<Input name="p4" type="radio" value="r2" label="Si elimino un archivo, lo puedo recuperar al moverme un estado anterior del proyecto" />
+						<Input name="p4" type="radio" value="r3" label="Hay 3 estados que conforman el flujo de trabajo local de git" />
+						<Input name="p4" type="radio" value="r4" label="Git guarda el nombre del archivo y el código hash de sus contenidos para identifar los cambios" />
 					</div>
 					<div>
 						<p>La clave SHA-1 se compone de:</p>
-						<div>
-							<label htmlFor="p5r1">40 caracteres</label>
-							<input type="radio" name="p5" value="r1" id="p5r1" checked />
-						</div>
-						<div>
-							<label htmlFor="p5r2">41 caracteres</label>
-							<input type="radio" name="p5" value="r2" id="p5r2" />
-						</div>
-						<div>
-							<label htmlFor="p5r3">44 caracteres</label>
-							<input type="radio" name="p5" value="r3" id="p5r3" />
-						</div>
-						<div>
-							<label htmlFor="p5r4">Todas las anteriores</label>
-							<input type="radio" name="p5" value="r4" id="p5r4" />
-						</div>
+						<Input name="p5" type="radio" value="r1" label="40 caracteres" defaultChecked='checked' />
+						<Input name="p5" type="radio" value="r2" label="41 caracteres" />
+						<Input name="p5" type="radio" value="r3" label="44 caracteres" />
+						<Input name="p5" type="radio" value="r4" label="Todas las anteriores" />
 					</div>
 					<input type="submit" value="Listo" />
 				</form>
